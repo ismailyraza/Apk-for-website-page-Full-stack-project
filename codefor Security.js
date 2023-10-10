464b253b832f2fd17447d3f363845e8380a4ed28
ismailyraza <98678304+ismailyraza@users.noreply.github.com>
2023-10-10 10:58:51 +0530

    Create codefor Security.js

---
+++ codefor Security.js
@@ -0,0 +1,65 @@
+const helmet = require('helmet');
+app.use(helmet());
+const { body, validationResult } = require('express-validator');
+
+app.post(
+  '/create-user',
+  [
+    body('name').isAlpha(),
+    body('email').isEmail(),
+    body('phone').isNumeric(),
+    // Add validation rules for other fields
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // Proceed with creating the user
+  }
+);
+const passport = require('passport');
+
+// Protect a route with authentication
+app.get('/protected', passport.authenticate('jwt', { session: false }), (req, res) => {
+  // Only authenticated users can access this route
+  res.json({ message: 'Authenticated route' });
+});
+const bcrypt = require('bcrypt');
+
+// Hash a password before storing it
+const saltRounds = 10;
+bcrypt.hash('userPassword', saltRounds, (err, hash) => {
+  if (err) throw err;
+  // Store the hash in the database
+});
+const helmet = require('helmet');
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", 'trusted-cdn.com'],
+      // Add other CSP directives as needed
+    },
+  })
+);
+const rateLimit = require('express-rate-limit');
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+});
+app.use('/api', limiter);
+const https = require('https');
+const fs = require('fs');
+
+const privateKey = fs.readFileSync('path/to/private-key.pem', 'utf8');
+const certificate = fs.readFileSync('path/to/certificate.pem', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+
+const httpsServer = https.createServer(credentials, app);
+app.use((req, res, next) => {
+  if (req.headers['content-type'] !== 'application/json') {
+    return res.status(400).json({ message: 'Invalid content type' });
+  }
+  next();
+});
